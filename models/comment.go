3459e34eb53cbc7bf36774718e3b19d99ef2902a
Aserck-ck <85006426+Aserck-ck@users.noreply.github.com>
2023-02-19 23:42:48 +0800

    Update comment.go

--- models/comment.go
+++ models/comment.go
@@ -8,8 +8,8 @@ import (
 
 type Comment struct {
 	Id         int64     `json:"id"`
-	UserInfoId int64     `json:"-"` //用于一对多关系的id
-	VideoId    int64     `json:"-"` //一对多，视频对评论
+	UserInfoId int64     `json:"-"` 
+	VideoId    int64     `json:"-"` 
 	User       UserInfo  `json:"user" gorm:"-"`
 	Content    string    `json:"content"`
 	CreatedAt  time.Time `json:"-"`
@@ -31,11 +31,9 @@ func (c *CommentDAO) AddCommentAndUpdateCount(comment *Comment) error {
 	if comment == nil {
 		return errors.New("AddCommentAndUpdateCount comment空指针")
 	}
-	//执行事务
 	return DB.Transaction(func(tx *gorm.DB) error {
 		//添加评论数据
 		if err := tx.Create(comment).Error; err != nil {
-			// 返回任何错误都会回滚事务
 			return err
 		}
 		//增加count
@@ -43,38 +41,34 @@ func (c *CommentDAO) AddCommentAndUpdateCount(comment *Comment) error {
 			return err
 		}
 
-		// 返回 nil 提交事务
 		return nil
 	})
 }
 
 func (c *CommentDAO) DeleteCommentAndUpdateCountById(commentId, videoId int64) error {
-	//执行事务
 	return DB.Transaction(func(tx *gorm.DB) error {
 		//删除评论
 		if err := tx.Exec("DELETE FROM comments WHERE id = ?", commentId).Error; err != nil {
-			// 返回任何错误都会回滚事务
 			return err
 		}
 		//减少count
 		if err := tx.Exec("UPDATE videos v SET v.comment_count = v.comment_count-1 WHERE v.id=? AND v.comment_count>0", videoId).Error; err != nil {
 			return err
 		}
-		// 返回 nil 提交事务
 		return nil
 	})
 }
 
 func (c *CommentDAO) QueryCommentById(id int64, comment *Comment) error {
 	if comment == nil {
-		return errors.New("QueryCommentById comment 空指针")
+		return errors.New("QueryCommentById comment nullptr")
 	}
 	return DB.Where("id=?", id).First(comment).Error
 }
 
 func (c *CommentDAO) QueryCommentListByVideoId(videoId int64, comments *[]*Comment) error {
 	if comments == nil {
-		return errors.New("QueryCommentListByVideoId comments空指针")
+		return errors.New("QueryCommentListByVideoId comments nullptr")
 	}
 	if err := DB.Model(&Comment{}).Where("video_id=?", videoId).Find(comments).Error; err != nil {
 		return err
