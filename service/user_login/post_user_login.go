24161ab00ec0c739038024b529c3343057de0761
Aserck-ck <85006426+Aserck-ck@users.noreply.github.com>
2023-02-21 17:17:03 +0800

    Update post_user_login.go

--- service/user_login/post_user_login.go
+++ service/user_login/post_user_login.go
@@ -2,11 +2,10 @@ package user_login
 
 import (
 	"errors"
-	"github.com/ACking-you/byte_douyin_project/middleware"
-	"github.com/ACking-you/byte_douyin_project/models"
+	"github.com/Aserck-ck/tiktok/middleware"
+	"github.com/Aserck-ck/tiktok/models"
 )
 
-// PostUserLogin 注册用户并得到token和id
 func PostUserLogin(username, password string) (*LoginResponse, error) {
 	return NewPostUserLoginFlow(username, password).Do()
 }
@@ -25,17 +24,13 @@ type PostUserLoginFlow struct {
 }
 
 func (q *PostUserLoginFlow) Do() (*LoginResponse, error) {
-	//对参数进行合法性验证
 	if err := q.checkNum(); err != nil {
 		return nil, err
 	}
-
-	//更新数据到数据库
 	if err := q.updateData(); err != nil {
 		return nil, err
 	}
 
-	//打包response
 	if err := q.packResponse(); err != nil {
 		return nil, err
 	}
@@ -57,24 +52,18 @@ func (q *PostUserLoginFlow) checkNum() error {
 
 func (q *PostUserLoginFlow) updateData() error {
 
-	//准备好userInfo,默认name为username
 	userLogin := models.UserLogin{Username: q.username, Password: q.password}
 	userinfo := models.UserInfo{User: &userLogin, Name: q.username}
 
-	//判断用户名是否已经存在
 	userLoginDAO := models.NewUserLoginDao()
 	if userLoginDAO.IsUserExistByUsername(q.username) {
 		return errors.New("用户名已存在")
 	}
-
-	//更新操作，由于userLogin属于userInfo，故更新userInfo即可，且由于传入的是指针，所以插入的数据内容也是清楚的
 	userInfoDAO := models.NewUserInfoDAO()
 	err := userInfoDAO.AddUserInfo(&userinfo)
 	if err != nil {
 		return err
 	}
-
-	//颁发token
 	token, err := middleware.ReleaseToken(userLogin)
 	if err != nil {
 		return err
