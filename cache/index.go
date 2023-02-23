1a041437a0440dbe862732c55c732d58f55d6f57
Aserck-ck <85006426+Aserck-ck@users.noreply.github.com>
2023-02-23 20:46:21 +0800

    Update index.go

--- cache/index.go
+++ cache/index.go
@@ -8,7 +8,6 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
-// 用户id->被点赞的视频id集合->是否含有该视频id
 
 var ctx = context.Background()
 var rdb *redis.Client
@@ -22,7 +21,7 @@ func init() {
 	rdb = redis.NewClient(
 		&redis.Options{
 			Addr:     fmt.Sprintf("%s:%d", config.Info.RDB.IP, config.Info.RDB.Port),
-			Password: "", //没有设置密码
+			Password: "", 
 			DB:       config.Info.RDB.Database,
 		})
 }
@@ -38,7 +37,6 @@ func NewProxyIndexMap() *ProxyIndexMap {
 	return &proxyIndexOperation
 }
 
-// UpdateVideoFavorState 更新点赞状态，state:true为点赞，false为取消点赞
 func (i *ProxyIndexMap) UpdateVideoFavorState(userId int64, videoId int64, state bool) {
 	key := fmt.Sprintf("%s:%d", favor, userId)
 	if state {
@@ -48,14 +46,12 @@ func (i *ProxyIndexMap) UpdateVideoFavorState(userId int64, videoId int64, state
 	rdb.SRem(ctx, key, videoId)
 }
 
-// GetVideoFavorState 得到点赞状态
 func (i *ProxyIndexMap) GetVideoFavorState(userId int64, videoId int64) bool {
 	key := fmt.Sprintf("%s:%d", favor, userId)
 	ret := rdb.SIsMember(ctx, key, videoId)
 	return ret.Val()
 }
 
-// UpdateUserRelation 更新点赞状态，state:true为点关注，false为取消关注
 func (i *ProxyIndexMap) UpdateUserRelation(userId int64, followId int64, state bool) {
 	key := fmt.Sprintf("%s:%d", relation, userId)
 	if state {
@@ -65,7 +61,6 @@ func (i *ProxyIndexMap) UpdateUserRelation(userId int64, followId int64, state b
 	rdb.SRem(ctx, key, followId)
 }
 
-// GetUserRelation 得到关注状态
 func (i *ProxyIndexMap) GetUserRelation(userId int64, followId int64) bool {
 	key := fmt.Sprintf("%s:%d", relation, userId)
 	ret := rdb.SIsMember(ctx, key, followId)
